4534ae4f0ee8bca81454171c50bc87ac04dd2b6a
desen <>
2022-03-29 15:16:37 +0800

    test

---
+++ js/instanceof.js
@@ -0,0 +1,41 @@
+/**
+ * instanceof 判断一个对象是否是另一个对象（构造函数）的实例，返回boolean
+ */
+
+
+
+class Base { }
+
+class A extends Base {
+    constructor() {
+        super()
+    }
+}
+
+let a = new A();
+
+console.log('a instanceof A ', a instanceof A);
+console.log('a instanceof Base ', a instanceof Base);
+
+/**
+ * obj.__proto__ === constructor.prototype
+ * @param {*} obj 
+ * @param {*} constructor 
+ * @returns 
+ */
+
+function _instanceof(obj, constructor) {
+    let left = obj.__proto__;
+    let right = constructor.prototype;
+    while (left) {
+        if (left === right) {
+            return true;
+        } else {
+            left = left.__proto__;
+        }
+    }
+    return false
+}
+
+console.log(_instanceof(a, A))
+console.log(_instanceof(a, Base))
